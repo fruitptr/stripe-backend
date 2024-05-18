e552180d6febf818ae823beae3530d6536cb7945
Shaheer <shaheerahmaddev@gmail.com>
2024-05-18 15:32:51 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const stripe = require('stripe')(process.env.secret_key);
+app.use(express.static('.'));
+app.use(express.json());
+
+app.post('/create-payment-intent', async (req, res) => {
+  let customer = await stripe.customers.create();
+  let { amount } = req.body;
+  console.log(amount);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount * 100,
+    currency: 'usd',
+    customer: customer.id
+  });
+  const clientSecret = paymentIntent.client_secret;
+
+  res.send({
+    clientSecret: clientSecret,
+    customer: customer.id
+  });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Node server listening on port ${process.env.PORT}!`)
+);
